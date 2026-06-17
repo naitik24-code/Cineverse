d92f7315c69e6331ea033e283045de6390b8c4b3
naitik24-code <naitikpathak2005@gmail.com>
2026-06-18 00:33:05 +0530

    Configure manual MongoClient beans to guarantee connection to MongoDB Atlas

--- backend/review-service/src/main/java/com/cineverse/review/ReviewServiceApplication.java
+++ backend/review-service/src/main/java/com/cineverse/review/ReviewServiceApplication.java
@@ -14,9 +14,8 @@ public class ReviewServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		System.out.println("=== STARTING REVIEW-SERVICE ===");
+		System.out.println("All available Env Var Keys: " + System.getenv().keySet());
 		System.out.println("SPRING_DATA_MONGODB_URI env: " + System.getenv("SPRING_DATA_MONGODB_URI"));
-		System.out.println("SPRING_DATA_MONGODB_URL env: " + System.getenv("SPRING_DATA_MONGODB_URL"));
-		System.out.println("MONGODB_URI env: " + System.getenv("MONGODB_URI"));
 		System.out.println("=================================");
 		SpringApplication.run(ReviewServiceApplication.class, args);
 	}
